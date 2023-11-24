9f0743aaf69c3d44b6ee3ad7fa0567c1538e2e83
Felipe <139351148+felipopm@users.noreply.github.com>
2023-11-23 22:01:26 -0300

    Enviando projeto para deploy

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Container from './componente/Container'
+
+function App() {
+
+  return (
+    <>
+      <Container />
+    </>
+  )
+}
+
+export default App
---
+++ src/componente/Container/index.jsx
@@ -0,0 +1,8 @@
+function Container() {
+    return(
+        <section>
+            <h1>Teste</h1>
+        </section>
+    );
+}
+export default Container;
